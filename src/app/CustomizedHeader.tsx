3047e2252283141f7e2a6e911e9ea9344b323d2a
ZhenyuanGao <kegao@westmont.edu>
2024-08-14 15:14:17 -0700

    add extra div for antd registry

--- src/app/CustomizedHeader.tsx
+++ src/app/CustomizedHeader.tsx
@@ -2,18 +2,13 @@
 'use client'
 
 //import Header from '../app/Header'
-import { Button, Card, Divider, Flex, Image, Layout, Space, Typography,ConfigProvider, Row, Col } from "antd"
+import { Button, Flex, Layout } from "antd"
 import React ,{useContext}from "react"
-import {AntDesignOutlined, QuestionCircleOutlined, QuestionOutlined} 
+import {AntDesignOutlined} 
 from '@ant-design/icons'
-import dynamic from 'next/dynamic'
-import { css ,cx} from '@emotion/css'
-import SiteFooter from "./Footer"
 import {FaXTwitter, FaInstagram, FaSlack, FaYoutube} from "react-icons/fa6"
-import MainPageContent from "./MainPageContent"
 
 const { Header, Footer, Sider, Content } = Layout;
-const {Text,Title} = Typography;
 const headerStyle: React.CSSProperties = {
 
  backgroundColor:'white'
@@ -23,48 +18,8 @@ const headerStyle: React.CSSProperties = {
 
 const CustimizedHeader = ()=>{
 
-    const { getPrefixCls } = useContext(ConfigProvider.ConfigContext);
-    const rootPrefixCls = getPrefixCls();
-    const linearGradientButton = css`
-      &.${rootPrefixCls}-btn-primary:not([disabled]):not(.${rootPrefixCls}-btn-dangerous) {
-        border-width: 0;
-  
-        > span {
-          position: relative;
-        }
-  
-        &::before {
-          content: '';
-          background: linear-gradient(135deg, #6253E1, #04BEFE);
-          position: absolute;
-          inset: 0;
-          opacity: 1;
-          transition: all 0.3s;
-          border-radius: inherit;
-        }
-  
-        &:hover::before {
-          opacity: 0;
-        }
-      }
-    ` ;
-    //const nonNullClassName: string = linearGradientButton as string;
-    const SignInMessage =  `Can't wait, please let us know more about you`;
-    const ChurchMessage = 'We help you start new churches that thrive, grow, and multiply';
-    const imgStyle: React.CSSProperties = {
-      display: 'block',
-      maxWidth: 473,
-      transform: 'rotate(10deg)', 
-    border: '2px solid #000', 
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-    };
-    const imgStyleRev: React.CSSProperties = {
-      display: 'block',
-      maxWidth: 473,
-      transform: 'rotate(-10deg)', 
-    border: '2px solid #000', 
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-    };
+   
+   
     const socials = [
           {
               title: "X (Twitter)",
